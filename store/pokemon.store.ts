65fdc977beb3f124b030fe14b1d0b26430fec3fa
Lucas <lucasbastos@ilove.me>
2022-04-09 02:22:44 -0300

    feat: basic structure

--- store/pokemon.store.ts
+++ store/pokemon.store.ts
@@ -1,13 +1,32 @@
 import { defineStore } from "pinia";
 import * as Service from '../services'
 
-const state = () => ({});
+export type Pokemon = {
+    name: string;
+    url: string;
+    types: string[];
+    image: string;
+}
+
+const state = () => ({
+    pokemons: [] as Pokemon[],
+});
 const getters = {};
 const actions = {
     async getPokemonAll() {
         const response = await Service.getPokemonAll()
-        return response.results
-    }
+        response.results.forEach(async (pokemon, index) => {
+            const types = await Service.getPokemonInfo(pokemon.name)
+            this.state.pokemons.push({
+                name: pokemon.name,
+                url: pokemon.url,
+                types: [],
+                image: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${index+1}.png`
+            })
+        })
+    },
 };
+
+
 
 export const usePokemonStore = defineStore("pokemonStore", {state, getters, actions});
